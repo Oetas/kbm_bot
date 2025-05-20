e6dece21477c82edd80df4f32fd073cb1cf616b3
Glebious <sprpmail@gmail.com>
2025-05-20 08:37:12 +0300

    Создан файл main.py с подключением бота

---
+++ main.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from handlers import start_command, help_command, about_command
+import os
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+app = ApplicationBuilder().token("7404512928:AAG6ZfRiwe7KongTrdY6dsIJnuv-Nu1SoKg").build()
+
+app.add_handler(CommandHandler("start", start_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("about", about_command))
+
+print("Бот запущен!")
+app.run_polling()
