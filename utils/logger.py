572c174a66299baa53fb0743e7c3c07a73b9a69d
Glebious <sprpmail@gmail.com>
2025-05-20 08:38:12 +0300

    обавлен логгер событий

---
+++ utils/logger.py
@@ -0,0 +1,6 @@
+import datetime
+
+def log_event(user_id, message):
+    with open("logs/bot.log", "a", encoding="utf-8") as f:
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        f.write(f"[{time}] Пользователь {user_id}: {message}\n")
